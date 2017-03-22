0bccc3a7405b7f3c4e0ca94ee4422af8dcd64d77
Robert <rbtwjohnson@gmail.com>
2017-03-21 23:16:19 -0400

    added user authentication

--- account/views.py
+++ account/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+
 from .models import Profile
 
 
