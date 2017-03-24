844aa7fee04330200a0300a78a7ea61769740f23
Robert <rbtwjohnson@gmail.com>
2017-03-23 22:49:49 -0400

    mar 23 update

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+import django.contrib.auth
 
 from . import views
 
 
 urlpatterns = [
-    #url(r'^login/$', views.login, name='login'),
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
     url(r'^$', views.dashboard, name = 'dashboard'),
+    #url(r'^login/$', views.user_login, name='login'),
+    url(r'^login/$' , django.contrib.auth.login, name='login'),
+    url(r'^logout/$', django.contrib.auth.logout, name='logout'),
+    #url(r'^logout-then-login/$', django.contrib.auth.logout_then_login, name='logout_then_login'),
+    url(r'^register/$', views.register, name='register'),
+
 
 ]
